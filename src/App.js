ec00b2c732b5f6c788b10ce1d620145a5635bd7e
principlebrothers <principlesbrothers@yahoo.com>
2022-08-11 18:13:32 +0000

    Install router dom and set route for the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Nav from './pages/Nav';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
