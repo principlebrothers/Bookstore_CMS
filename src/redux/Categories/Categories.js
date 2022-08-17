bc4772b19832d0ac1e3238af6470b054ea912632
principlebrothers <principlesbrothers@yahoo.com>
2022-08-17 13:54:37 +0000

    Add categories reducer and action creator to the Categories.js file in the redux repository

---
+++ src/redux/Categories/Categories.js
@@ -0,0 +1,17 @@
+// Actions
+const CHECK_STATUS = 'bookstore/Categories/CHECK_STATUS';
+
+// Reducer
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
+
+// Action Creator
+export const checkStatus = () => ({ type: CHECK_STATUS });
+
+export default categoriesReducer;
