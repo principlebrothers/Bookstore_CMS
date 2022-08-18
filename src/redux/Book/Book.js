a7a90b4cf8619ea1cbd006b1010ec0c4c23275ef
principlebrothers <principlesbrothers@yahoo.com>
2022-08-18 13:22:03 +0000

    Add book array as an initial state array

--- src/redux/Book/Book.js
+++ src/redux/Book/Book.js
@@ -1,11 +1,53 @@
+import { v4 as uuidv4 } from 'uuid';
 // Actions
 const ADD_BOOK = 'bookstore/Books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/Books/REMOVE_BOOK';
 
-const initialState = [];
+const books = [
+  {
+    id: uuidv4(),
+    title: 'Please Understand Me II',
+    author: 'David Kerisey',
+    progress: 70,
+    genre: 'Psychology',
+    chapter: 7,
+  },
+  {
+    id: uuidv4(),
+    title: 'Do not pack your bags yet',
+    author: 'Abel Damina',
+    progress: 100,
+    genre: 'Life',
+    chapter: 10,
+  },
+  {
+    id: uuidv4(),
+    title: 'Person of Interest',
+    author: 'Jonathan Nolan',
+    progress: 80,
+    genre: 'Science Fiction',
+    chapter: 8,
+  },
+  {
+    id: uuidv4(),
+    title: 'Rich Dad Poor Dad',
+    author: 'Robert Kiyosaki',
+    progress: 20,
+    genre: 'Finance',
+    chapter: 5,
+  },
+  {
+    id: uuidv4(),
+    title: 'The pupose of Love and Marriage',
+    author: 'Myles Munroe',
+    progress: 10,
+    genre: 'Relationship',
+    chapter: 2,
+  },
+];
 
 // Reducer
-const bookReducer = (state = initialState, action) => {
+const bookReducer = (state = books, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.book];
